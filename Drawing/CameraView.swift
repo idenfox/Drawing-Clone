1f2f8ce8e61a153726160eb9bfd730672ab7528a
Renzo Paul Chamorro <renzo607@gmail.com>
2021-04-22 17:45:51 -0500

    Pre Alpha Version

---
+++ Drawing/CameraView.swift
@@ -0,0 +1,86 @@
+//
+//  CameraView.swift
+//  Drawing
+//
+//  Created by Renzo Paul Chamorro on 22/04/21.
+//
+
+import Foundation
+import SwiftUI
+import AVFoundation
+
+struct CameraView: View {
+    @StateObject var camera = CameraViewModel()
+    var body: some View{
+        CameraPreview(camera: camera)
+            .onAppear(perform: {
+                camera.checkPermissions()
+            })
+    }
+}
+
+struct CameraView_preview: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
+
+class CameraViewModel: NSObject, ObservableObject, AVCapturePhotoCaptureDelegate  {
+    
+    @Published var session = AVCaptureSession()
+    @Published var alert: Bool = false
+    @Published var preview = AVCaptureVideoPreviewLayer()
+    
+    func checkPermissions() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            setupCamera()
+            return
+        case .denied:
+            self.alert = true
+            return
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { (acepted) in
+                if acepted {
+                    self.setupCamera()
+                }
+            }
+        default:
+            return
+        }
+    }
+    
+    func setupCamera() {
+        do {
+            session.beginConfiguration()
+            let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+            let input = try AVCaptureDeviceInput(device: device!)
+            if self.session.canAddInput(input){
+                self.session.addInput(input)
+            }
+            self.session.commitConfiguration()
+        } catch {
+            print("Error al configurar camara: \(error.localizedDescription)")
+        }
+    }
+}
+
+struct CameraPreview: UIViewRepresentable {
+    @ObservedObject var camera: CameraViewModel
+    
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView(frame: UIScreen.main.bounds)
+        
+        camera.preview = AVCaptureVideoPreviewLayer(session: camera.session)
+        camera.preview.frame = view.frame
+        
+        camera.preview.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(camera.preview)
+        
+        camera.session.startRunning()
+        
+        return view
+    }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+}
--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,14 +8,102 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var image: UIImage?
+    @State var shouldShowImagePIcker: Bool = false
+     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Button(action: {
+                    shouldShowImagePIcker = true
+                }, label: {
+                    Text("Elige foto")
+                })
+                NavigationLink(
+                    destination: SecondView(image: $image),
+                    label: {
+                        Text("Entrar")
+                    })
+                    .padding()
+                    .navigationTitle("Drawing")
+            }
+        }.sheet(isPresented: $shouldShowImagePIcker, content: {
+            ImagePicker(image: $image, isShown: $shouldShowImagePIcker)
+        })
+    }
+
+}
+
+struct SecondView: View {
+    
+    @Binding var image: UIImage?
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            CameraView()
+            Image(uiImage: image ?? UIImage(named: "placeholder")!)
+                .resizable()
+                .scaledToFill()
+                .rotationEffect(.degrees(-90))
+                .opacity(0.25)
+        }
     }
 }
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var image: UIImage?
+    @Binding var isShown: Bool
+
+    typealias UIViewControllerType = UIImagePickerController
+    typealias Coordinator = ImagePickerCoordinator
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        
+        let picker = UIImagePickerController()
+        let sourceType: UIImagePickerController.SourceType = .photoLibrary
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+
+    }
+    
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        return ImagePickerCoordinator(image: $image, isShown: $isShown)
+    }
+
+}
+
+class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @Binding var image: UIImage?
+    @Binding var isShown: Bool
+    
+    init(image: Binding<UIImage?>, isShown: Binding<Bool>) {
+        self._image = image
+        self._isShown = isShown
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.image = uiImage
+            self.isShown = false
+        }
+    }
+    
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+

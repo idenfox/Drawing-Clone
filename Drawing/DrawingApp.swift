427a20ba8df2d5cd6a95668dc0af61829b185502
Renzo Paul Chamorro <renzo607@gmail.com>
2021-04-22 13:06:51 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Renzo Paul Chamorro on 22/04/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
